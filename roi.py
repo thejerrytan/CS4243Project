8ff09ffeb876a7791e02634b2d22a1732c598a86
Jerry Tan <jerrytansk@gmail.com>
2016-11-05 21:01:01 +0800

    refactor ROI to new file

---
+++ roi.py
@@ -0,0 +1,55 @@
+CLIP1_GREEN_P1_ROI = {
+	'x' : 330,
+	'y' : 180,
+	'h' : 50,
+	'w' : 50
+}
+# CLIP1_GREEN_P1_ROI = {
+# 	'x' : 470,
+# 	'y' : 210,
+# 	'h' : 30,
+# 	'w' : 30
+# }
+CLIP1_GREEN_P2_ROI = {
+	'x' : 190,
+	'y' : 100,
+	'h' : 50,
+	'w' : 50
+}
+
+CLIP1_VCOURT_BOT_RIGHT = {
+	'x' : 430,
+	'y' : 130,
+	'h' : 50,
+	'w' : 50
+}
+CLIP1_VCOURT_BOT_LEFT = {
+	'x' : 170,
+	'y' : 275,
+	'h' : 50,
+	'w' : 50
+}
+CLIP1_VCOURT_NET_RIGHT = {
+	'x' : 255,
+	'y' : 70,
+	'h' : 50,
+	'w' : 50
+}
+CLIP1_VCOURT_NET_LEFT = {
+	'x' : 35,
+	'y' : 130,
+	'h' : 50,
+	'w' : 50
+}
+CLIP1_VCOURT_RIGHT_MID = {
+	'x' : 280,
+	'y' : 85,
+	'h' : 10,
+	'w' : 10
+}
+CLIP1_VCOURT_LEFT_MID = {
+	'x' : 70,
+	'y' : 130,
+	'h' : 15,
+	'w' : 15
+}
